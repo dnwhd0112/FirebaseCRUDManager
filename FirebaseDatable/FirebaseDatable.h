b9cde5baf9fbd8c5ce801335e12632a90d488b88
dnwhd0112 <dnwhd0112@naver.com>
2023-01-20 10:28:56 +0900

    Initial Commit

---
+++ FirebaseDatable/FirebaseDatable.h
@@ -0,0 +1,18 @@
+//
+//  FirebaseDatable.h
+//  FirebaseDatable
+//
+//  Created by 곽우종 on 2023/01/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FirebaseDatable.
+FOUNDATION_EXPORT double FirebaseDatableVersionNumber;
+
+//! Project version string for FirebaseDatable.
+FOUNDATION_EXPORT const unsigned char FirebaseDatableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FirebaseDatable/PublicHeader.h>
+
+
